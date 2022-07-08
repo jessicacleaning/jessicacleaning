56885dfc259b9fffed42608b3d867b70f97737a5
Namig Hajiyev <namiq886@gmail.com>
2022-07-08 19:08:14 +0400

    optimize build files

--- gulpfile.js
+++ gulpfile.js
@@ -2,21 +2,32 @@ const { series, src, dest } = require('gulp');
 const rimraf = require('gulp-rimraf');
 var revAll = require("gulp-rev-all");
 var revdel = require('gulp-rev-delete-original');
+const sass = require('gulp-sass')(require('sass'));
+const autoprefixer = require('gulp-autoprefixer');
+const htmlmin = require('gulp-htmlmin');
+const gulpif = require('gulp-if');
+const cleanCSS = require('gulp-clean-css');
+const lazypipe = require('lazypipe');
+const babel = require('gulp-babel');
+const uglify = require('gulp-uglify');
+
 
 
 const srcPath = 'src';
 const destPath = 'dist';
 
-function clean() {
+function cleanTask() {
     return src(destPath, { read: false, allowEmpty: true }) // much faster
         .pipe(rimraf());
 }
 
-function build(cb) {
-    cb();
+function sassTask() {
+    return src(`${srcPath}/scss/style.scss`)
+        .pipe(sass().on('error', sass.logError))
+        .pipe(dest(`${srcPath}/css`));
 }
 
-function copyAll() {
+function copyAllTask() {
     return src(`${srcPath}/**/*`)
         .pipe(dest(destPath));
 }
@@ -35,9 +46,32 @@ function revAllTask() {
         .pipe(dest(destPath));
 }
 
+function optimizeAllTask() {
+    const optimizeHtml = lazypipe()
+        .pipe(() => htmlmin({ collapseWhitespace: true }))
 
-exports.build = build;
-exports.clean = clean;
-exports.copyAll = copyAll;
-exports.revAll = series(clean, copyAll, revAllTask);
-exports.default = series(clean, build);
+    const optimizeCss = lazypipe()
+        .pipe(() => cleanCSS({ compatibility: 'ie8' }))
+        .pipe(autoprefixer);
+
+    const optimizeJs = lazypipe()
+        // .pipe(() => babel({
+        //     presets: ['@babel/env']
+        // }))
+        .pipe(() => uglify())
+
+
+    return src(`${destPath}/**/*`)
+        .pipe(gulpif('*.html', optimizeHtml()))
+        .pipe(gulpif('*.css', optimizeCss()))
+        .pipe(gulpif('*.js', optimizeJs()))
+        .pipe(dest(destPath));
+}
+
+exports.build = series(cleanTask, sassTask, copyAllTask, optimizeAllTask, revAllTask);
+exports.clean = cleanTask;
+exports.copyAll = copyAllTask;
+exports.sass = sassTask;
+exports.revAll = series(cleanTask, copyAllTask, revAllTask);
+exports.optimize = series(cleanTask, copyAllTask, optimizeAllTask);
+exports.default = series(cleanTask);
--- src/js/main.js
+++ src/js/main.js
@@ -124,7 +124,6 @@
                 $('.number').each(function () {
                     var $this = $(this),
                         num = $this.data('number');
-                    console.log(num);
                     $this.animateNumber(
                         {
                             number: num,
